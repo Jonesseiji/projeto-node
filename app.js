e5c6ef2bb1ab1c7309c79e13d62b4e1099fa5f3b
Jones Seiji <jonesseiji@outlook.com>
2021-10-05 14:55:18 -0300

    disponibilizando arquivos estáticos e incluindo url de autodownload do arquivo quando acessado

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import livrosRoute from "./routes/livrosRoute.js";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import path from "path";
 
 const PORTA = 3000;
 const server = express();
@@ -10,8 +11,13 @@ const criarUrl = (version, path) => `/api/${version}/${path}`;
 const LIVROS_URL = criarUrl("v1", "livros");
 
 server.use(morgan("tiny"));
+server.use("/static", express.static("public"));
 server.use(bodyParser.json());
 server.use(LIVROS_URL, livrosRoute);
+
+server.get("/download/images/:imageName", (req, res) => {
+    res.download(path.join("public", "images", req.params.imageName)) //faz o download da imagem no caminho localhost:3000/download/static/images/nome da imagem.extensão(jpg,png)
+})
 
 server.get("/manipulando-rota", (req, res, next) =>{
     res.send("Aprendendo route handler");
