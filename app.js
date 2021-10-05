a697ea6cde8f188403b5ace71b828084e7167748
Jones Seiji <jonesseiji@outlook.com>
2021-10-05 15:07:01 -0300

    utilizando EJS e template engine

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import livrosRoute from "./routes/livrosRoute.js";
+import livros from "./data/livros.json"
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import path from "path";
@@ -10,14 +11,23 @@ const server = express();
 const criarUrl = (version, path) => `/api/${version}/${path}`;
 const LIVROS_URL = criarUrl("v1", "livros");
 
+server.set("views", path.join("views"));
+server.set("view engine", "ejs")
+
 server.use(morgan("tiny"));
 server.use("/static", express.static("public"));
 server.use(bodyParser.json());
 server.use(LIVROS_URL, livrosRoute);
 
+server.get("/", (req, res) => {
+    res.render("index", {
+        livros: livros
+    });
+});
+
 server.get("/download/images/:imageName", (req, res) => {
-    res.download(path.join("public", "images", req.params.imageName)) //faz o download da imagem no caminho localhost:3000/download/static/images/nome da imagem.extensão(jpg,png)
-})
+    res.download(path.join("public", "images", req.params.imageName)); //faz o download da im"agem no caminho localhost:3000/download/static/images/nome da imagem.extensão(jpg,png)
+});
 
 server.get("/manipulando-rota", (req, res, next) =>{
     res.send("Aprendendo route handler");
